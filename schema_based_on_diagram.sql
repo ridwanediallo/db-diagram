6295ada9431f564899e1137cce944a6f748df448
unknown <sntakirutimana72@gmail.com>
2022-08-16 19:24:27 +0200

    Add join table to handle many to many relationship

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,45 +1,53 @@
-CREATE TABLE "patients" (
-  "id"      int,
-  "name"    varchat(100),
-  "date_of_birth" date
+CREATE TABLE patients (
+  id            int generated always as identity,
+  name          varchar(100),
+  date_of_birth date,
+  primary key(id)
 );
 
-CREATE TABLE "invoices" (
-  "id"                   int,
-  "total_amont"          decimal,
-  "generated_at"         timestamp,
-  "paid_at"              timestamp,
-  "medicall_history_id"  int
+CREATE TABLE medical_histories (
+  id            int generated always as identity,
+  admitted_at   timestamp,
+  patient_id    int,
+  status        varchar(100),
+  primary key(id),
+  FOREIGN KEY (patient_id) REFERENCES patients (id)
 );
 
-CREATE TABLE "medicale_histories" (
-  "id"            int,
-  "admitted_at "  timestamp,
-  "patient_id "   int,
-  "status" varchat(100),
-  CONSTRAINT "FK_medicale_histories.patient_id"
-    FOREIGN KEY ("patient_id")
-      REFERENCES "patients"("id")
+CREATE TABLE invoices (
+  id                   int generated always as identity,
+  total_amount         decimal,
+  generated_at         timestamp,
+  payed_at             timestamp,
+  medical_history_id   int,
+  primary key(id),
+  foreign key (medical_history_id) references medical_histories(id)
 );
 
-CREATE TABLE "treatments" (
-  "id"   int,
-  "type" varchat(100),
-  "name" varchat(100)
+CREATE TABLE treatments (
+  id   int generated always as identity,
+  type varchar(100),
+  name varchar(100),
+  primary key(id)
 );
 
-CREATE TABLE "invoice_items" (
-  "id"             int,
-  "unit_price"     decimal,
-  "quantity "      int,
-  "total_price "   decimal,
-  "invoice_id  "   int,
-  "treatment_id "  int,
-  CONSTRAINT "FK_invoice_items.invoice_id"
-    FOREIGN KEY ("invoice_id")
-      REFERENCES "invoices"("id"),
-  CONSTRAINT "FK_invoice_items.treatment_id"
-    FOREIGN KEY ("treatment_id")
-      REFERENCES "treatments"("id")
+CREATE TABLE medical_histories_treatments (
+  treatment_id       int,
+  medical_history_id int,
+  primary key (treatment_id, medical_history_id),
+  foreign key (treatment_id) references treatments (id),
+  foreign key (medical_history_id) references medical_histories (id)
+);
+
+CREATE TABLE invoice_items (
+  id             int generated always as identity,
+  unit_price     decimal,
+  quantity       int,
+  total_price    decimal,
+  invoice_id     int,
+  treatment_id   int,
+  primary key(id),
+  FOREIGN KEY (invoice_id) REFERENCES invoices (id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments (id)
 );
 
